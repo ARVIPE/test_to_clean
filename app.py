873a7f1d58f73e8b49f4c0b964b3ac7af26f16c2
Arturo Vicente Pérez <55533930+ARVIPE@users.noreply.github.com>
2025-12-30 14:25:07 +0100

    first: commit

---
+++ app.py
@@ -0,0 +1,12 @@
+def function():
+    API_KEY = "your_api_key_here"
+
+
+def function_example():
+    print("This is an example function.")
+    return "Function executed successfully."
+
+
+if __name__ == "__main__":
+    result = function_example()
+    print(result)
